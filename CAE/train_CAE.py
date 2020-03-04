3aa3330d48bf94de69f551d8ba02905c164188cb
dytrong <40820475+dytrong@users.noreply.github.com>
2020-03-04 11:58:06 +0800

    Update train_CAE.py

--- CAE/train_CAE.py
+++ CAE/train_CAE.py
@@ -15,19 +15,28 @@ torch.cuda.manual_seed(1)
 
 
 def select_pretrained_model(model_type, dimension):
+
     if model_type == 'alexnet':
+
         AutoEncoder = AutoEncoder_alexnet(dimension).cuda()
-    if model_type == 'vgg16':
-        AutoEncoder = AutoEncoder_vgg16(dimension).cuda()
+
     if model_type == 'resnet101':
+
         AutoEncoder = AutoEncoder_resnet101(dimension).cuda()
+
     if model_type == 'densenet169':
+
         AutoEncoder = AutoEncoder_densenet169(dimension).cuda()
+
     return AutoEncoder
 
 #####load model
 def autoencoder(model_parameters_path, model_type, dimension, pretrained=False):
+
     model = select_pretrained_model(model_type, dimension)
+
     if pretrained:
+
         model.load_state_dict(torch.load(model_parameters_path))
+
     return model
